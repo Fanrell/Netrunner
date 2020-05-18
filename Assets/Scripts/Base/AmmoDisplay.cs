855a353e4d899b0b4f2070622fea7f4f07d3594c
Krzysztof Kowalski <56967680+MrAraver@users.noreply.github.com>
2020-05-18 19:44:41 +0200

    zmianyKrzysztof

---
+++ Assets/Scripts/Base/AmmoDisplay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AmmoDisplay : MonoBehaviour
+{
+    private int ammo;
+    private PlayerBehavior player;
+    public bool isFiring;
+    public Text ammoDisplay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<PlayerBehavior>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ammo = player.curramo;
+        ammoDisplay.text = ammo.ToString() + " / 5";
+    }
+}
