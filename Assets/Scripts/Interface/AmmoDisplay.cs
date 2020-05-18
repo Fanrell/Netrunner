9bca00a98e7708945a418778295cb885cf8fa532
Krzysztof Kowalski <56967680+MrAraver@users.noreply.github.com>
2020-05-18 20:56:08 +0200

    amo i hp

--- Assets/Scripts/Interface/AmmoDisplay.cs
+++ Assets/Scripts/Interface/AmmoDisplay.cs
@@ -1,25 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Globalization;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class AmmoDisplay : MonoBehaviour
 {
-    private int ammo;
     private PlayerBehavior player;
-    public bool isFiring;
     public Text ammoDisplay;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player").GetComponent<PlayerBehavior>();
+        Debug.Log(player);
     }
 
     // Update is called once per frame
     void Update()
     {
-        ammo = player.curramo;
-        ammoDisplay.text = ammo.ToString() + " / 5";
+        ammoDisplay.text = "Ammo:\n" + player.curramo + " / " + player.maxamo;
     }
+
 }
--- Assets/Scripts/Interface/LifeDisplay.cs
+++ Assets/Scripts/Interface/LifeDisplay.cs
@@ -1,18 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class LifeDisplay : MonoBehaviour
 {
+    private PlayerBehavior player;
+    public Text lifeDisplay;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Player").GetComponent<PlayerBehavior>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        lifeDisplay.text = "Life:\n" + player.currhp + " / " + player.maxhp;
     }
 }
