168008bc9446b0f4b436b1d46153724fb461990c
Krzysztof Kowalski <56967680+MrAraver@users.noreply.github.com>
2020-06-27 21:28:48 +0200

    pauseMenuUpdate

--- Assets/Scripts/Interface/HealthBar.cs
+++ Assets/Scripts/Interface/HealthBar.cs
@@ -1,18 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
+    private PlayerBehavior player;
+    public Slider slider;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Player").GetComponent<PlayerBehavior>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //lifeDisplay.text = "Life:\n" + player.currhp + " / " + player.maxhp;
+
+    }
+
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;
     }
 }
